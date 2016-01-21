fe7c027c42ff7c1ba2b08077024513f44cfae7fa
Mark Harder <mark.harder899@gmail.com>
2016-01-21 16:01:12 -0600

    add a basic game loop

--- lib/ducky.rb
+++ lib/ducky.rb
@@ -3,7 +3,37 @@ require "ducky/version"
 module Ducky
   class DuckyGame
     def start
-      puts "Hello, world!"
+      clear
+      puts intro
+
+      loop do
+        print "> "
+        command = gets.chomp
+        clear
+
+        if command == "quit"
+          exit_game
+        end
+      end
+    end
+
+    def exit_game
+      puts "Game Over"
+      exit
+    end
+
+    private
+    def intro
+      <<~STR
+      DUCKY
+      --------
+      v#{VERSION}
+      
+      STR
+    end
+
+    def clear
+      print "\e[2J\e[24H"
     end
   end
 end
