11e052c0237dd758058223e3271aeff89b23c7af
Mark Harder <mark.harder899@gmail.com>
2016-02-12 12:02:29 +0900

    add basic movement

--- lib/ducky.rb
+++ lib/ducky.rb
@@ -20,6 +20,28 @@ module Ducky
           exit_game
         elsif command == "look"
           puts WORLD.room_at( PLAYER.location ).description
+        elsif command.start_with?( "go" )
+          x = PLAYER.location[0]
+          y = PLAYER.location[1]
+          z = PLAYER.location[2]
+
+          if command == "go north" && !WORLD.room_at([x, y+1, z]).nil?
+            puts "You go north."
+            PLAYER.location[1] += 1
+            puts WORLD.room_at( PLAYER.location ).description
+          elsif command == "go south" && !WORLD.room_at([x, y-1, z]).nil?
+            puts "You go south."
+            PLAYER.location[1] -= 1
+            puts WORLD.room_at( PLAYER.location ).description
+          elsif command == "go east" && !WORLD.room_at([x+1, y, z]).nil?
+            puts "You go east."
+            PLAYER.location[0] += 1
+            puts WORLD.room_at( PLAYER.location ).description
+          elsif command == "go west" && !WORLD.room_at([x-1, y, z]).nil?
+            puts "You go west."
+            PLAYER.location[0] -= 1
+            puts WORLD.room_at( PLAYER.location ).description
+          end
         end
       end
     end
